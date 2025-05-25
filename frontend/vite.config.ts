8fbc658afe63554b8a4e7e8198dac786ea9c58bd
diogenesdornelles <diogenes.dornelles@gmail.com>
2025-05-25 16:45:34 -0300

    refactor: :art: included vite aliases @

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@packages': path.resolve(__dirname, '../packages'),
+    },
+  },
 });
